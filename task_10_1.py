27404a8a20a086d3ad3ea8efea432c740b94db5a
LazySloth <Pavlinov.is.88@gmail.com>
2021-07-30 19:27:57 +0300

    10.1
    10.1

---
+++ task_10_1.py
@@ -0,0 +1,42 @@
+# 1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса
+# (метод __init__()), который должен принимать данные (список списков) для формирования матрицы.
+# Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+# Примеры матриц: 3 на 2, 3 на 3, 2 на 4.
+# | 31 22 |
+# | 37 43 |
+# | 51 86 |
+#
+# | 3 5 32 |
+# | 2 4 6 |
+# | -1 64 -8 |
+#
+# | 3 5 8 3 |
+# | 8 3 7 1 |
+
+from itertools import zip_longest
+
+
+def summa(x, y):
+    try:
+        return x + y
+    except:
+        return str(x) + str(y)
+
+
+class Matrix:
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return ''.join([f'{list(i)}\n' for i in zip(*self.data)])
+
+    def __add__(self, other):
+        return Matrix([[summa(x, y) for x, y in zip_longest(i, j, fillvalue=0)]
+                       for i, j in zip_longest(self.data, other.data, fillvalue=[])])
+
+
+m1 = Matrix([['a', 'b', 'c'], [1, 2, 3]])
+print(m1)
+m2 = Matrix([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
+print(m2)
+print(m2 + m1)
